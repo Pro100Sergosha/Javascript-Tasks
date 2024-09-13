2f605f322266fbe61442225dcc6af86c363a2828
Sergo Azizbekyan <prizrakgames21@gmail.com>
2024-09-13 20:09:15 +0400

    Week 6 task12 done

---
+++ week6/task12/task.js
@@ -0,0 +1,16 @@
+let strings = [
+  "ab",
+  "hello",
+  "world",
+  "code",
+  "js",
+  "frontend",
+  "react",
+  "css",
+  "dev",
+  "project",
+];
+
+let newArr = strings.sort((a, b) => a.length - b.length);
+
+console.log(newArr);
