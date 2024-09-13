bca07bf7cd3f075387103ffd4b9302af90d6d7cc
Sergo Azizbekyan <prizrakgames21@gmail.com>
2024-09-13 18:32:51 +0400

    Week6 task5 is ready

---
+++ week6/task5/task.js
@@ -0,0 +1,16 @@
+let users = [
+  { name: "Alice", age: 16 },
+  { name: "Bob", age: 17 },
+  { name: "Charlie", age: 18 },
+  { name: "Diana", age: 19 },
+  { name: "Eve", age: 20 },
+  { name: "Frank", age: 16 },
+  { name: "Grace", age: 17 },
+  { name: "Hank", age: 18 },
+  { name: "Ivy", age: 19 },
+  { name: "Jack", age: 20 },
+];
+
+let filteredUsers = users.map((user) => user.name);
+
+console.log(filteredUsers);
