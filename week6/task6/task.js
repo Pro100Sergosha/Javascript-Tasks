deab7cdf09ba3b26843136f5772ca16c27ef8ac7
Sergo Azizbekyan <prizrakgames21@gmail.com>
2024-09-13 18:34:42 +0400

    Week6 task6 is ready

---
+++ week6/task6/task.js
@@ -0,0 +1,16 @@
+let strings = [
+  "ab",
+  "hello",
+  "world",
+  "code",
+  "js",
+  "frontend",
+  "react",
+  "css",
+  "dev",
+  "project",
+];
+
+let changedStrings = strings.map((str) => str.toUpperCase());
+
+console.log(changedStrings);
