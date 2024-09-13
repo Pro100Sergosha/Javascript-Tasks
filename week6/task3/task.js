3daa03040bc20a0670efd79d054481911cb33c39
Sergo Azizbekyan <prizrakgames21@gmail.com>
2024-09-13 18:21:32 +0400

    Week6 task3 is ready

---
+++ week6/task3/task.js
@@ -0,0 +1,17 @@
+let strings = [
+    "ab",
+    "hello",
+    "world",
+    "code",
+    "js",
+    "frontend",
+    "react",
+    "css",
+    "dev",
+    "project"
+];
+
+
+let filteredStrings = strings.filter(str => str.length > 5)
+
+console.log(filteredStrings)
