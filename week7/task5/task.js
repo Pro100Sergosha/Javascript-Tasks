9bde6d52ed4ec7d0ac4787a94153c67cf70a0929
Sergo Azizbekyan <prizrakgames21@gmail.com>
2024-10-12 12:49:23 +0400

    Week 7 Task 5 done!

---
+++ week7/task5/task.js
@@ -0,0 +1,13 @@
+let Person = {
+  name: "Sergo",
+  "last name": "Azizbekyan",
+  age: 20,
+};
+
+Person.job = "Not working for now :(";
+
+Person.returnLength = function () {
+  return this.job.length;
+};
+
+console.log(Person.returnLength());
